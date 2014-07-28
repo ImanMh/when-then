c24c8a19b2f33c3e81d6758ef49458ca0b870c95
Iman Mohamadi <turbosianor@gmail.com>
2014-07-28 12:41:46 +0430

    converted to AMD format

--- whenthen.js
+++ whenthen.js
@@ -1,6 +1,7 @@
 //License: MIT License(http://opensource.org/licenses/mit-license.php)
 
-var when = function(){
+define( function () {
+  var when = function(){
     if( !(this instanceof when) ) return new when(arguments); //return new instance of itself
 
     var self = this; //cached so the syntax of code within the function is more readable
@@ -10,28 +11,33 @@ var when = function(){
     self.results = {length:0}; //container for results of async functions
 
     (function(){ // define pass() within this context so that the outer scope of self(this) is available when pass() is executed within the user's async functions
-        self.pass = function(){
+      self.pass = function(){
 
-            //self.results.push(arguments); //push async results to cache array
-            self.results[arguments[0]] = arguments[1];
-            self.results.length++;
+        //self.results.push(arguments); //push async results to cache array
+        self.results[arguments[0]] = arguments[1];
+        self.results.length++;
 
-            if( self.results.length === self.pending_length ) //if all async functions have finished, pass the results to .then(), which has been redefined to the user's completion function
-                self.then.call(self, self.results); 
-        };
+        if( self.results.length === self.pending_length ) //if all async functions have finished, pass the results to .then(), which has been redefined to the user's completion function
+          self.then.call(self, self.results); 
+      };
     })();
-}
-when.prototype = {
+  };
+  when.prototype = {
     then: function(){
-        
-        this.then = arguments[0]; //reassign .then() to the user-defined function that is executed on completion. Also ensures that this() can only be called once per usage of when()
-        
-        while(this.pending[0]){
-            this.pending.shift().call(this, this.pass); //execute all functions user passed into when()
-        }
+
+      this.then = arguments[0]; //reassign .then() to the user-defined function that is executed on completion. Also ensures that this() can only be called once per usage of when()
+
+      while(this.pending[0]){
+        this.pending.shift().call(this, this.pass); //execute all functions user passed into when()
+      }
 
     }
-}
+  };
 
+  return {
+    when: when,
+    then: then
+  };
+});
 
 
